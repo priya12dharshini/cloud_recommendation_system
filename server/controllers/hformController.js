e3c620aa780a200c5db7eceaff0f7331f48523dc
priya12dharshini <priya63599@gmail.com>
2024-01-18 00:37:10 +0530

    first commit

--- server/controllers/hformController.js
+++ server/controllers/hformController.js
@@ -17,7 +17,6 @@ const submitHealthcareForm = async (req, res) => {
     console.log('Request Body:', req.body);
     const { qn1, qn2, qn3, qn4, qn5, qn6, qn7, qn8, qn9, qn10 } = req.body;
 
-    // Creating an array of question-answer pairs
     const questionsAndAnswers = [
       { question: 'What tasks or activities do you want to accomplish using cloud services?', answer: qn1 },
       { question: 'Are you looking to enhance the efficiency of appointment scheduling and management?', answer: qn2 },
@@ -28,15 +27,13 @@ const submitHealthcareForm = async (req, res) => {
       { question: 'What is the expected geographical distribution of your users?', answer: qn7 },
       { question: 'Do you have any existing software?', answer: qn8 },
       { question: 'Would you like to implement patient engagement portals for feedback, reviews, and interaction?', answer: qn9 },
-      { question: 'Do you need a cloud service for remote patient monitoring, early warning systems, and alerts?', answer: qn10 }
+      { question: 'Do you need a cloud service for remote patient monitoring, early warning systems, and alerts?', answer: qn10 },
     ];
 
-    // Create a new HealthcareFormDataModel instance
     const hformData = new HealthcareFormDataModel({
       responses: questionsAndAnswers,
     });
 
-    // Save the data to MongoDB
     await hformData.save();
 
     res.status(201).json({ success: true, message: 'Healthcare Form data saved successfully' });
--- server/routes/hformRoutes.js
+++ server/routes/hformRoutes.js
@@ -1,13 +1,8 @@
-// hformRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const hformController = require('../controllers/hformController');
 
 router.post('/submitHealthcareForm', hformController.submitHealthcareForm);
-router.get('/test', (req, res) => {
-  res.send('Test route works!');
-});
 
 module.exports = router;
 
